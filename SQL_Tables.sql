5333a38264ccbc8e7142c63de8a0a336d91c422e
Tim McCabe <timmccabe44@gmail.com>
2020-01-25 12:50:58 -0500

    new

--- SQL_Tables.sql
+++ SQL_Tables.sql
@@ -6,4 +6,27 @@ CREATE TABLE Math_06-12 (
   mean_scale_score INT
 );
 
+CREATE TABLE ELA_06-12 (
+  DBN TEXT PRIMARY KEY,
+  Grade INT,
+  Year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
+CREATE TABLE Math_13-17 (
+  DBN TEXT PRIMARY KEY,
+  Grade INT,
+  Year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
+CREATE TABLE ELA_13-17 (
+  DBN TEXT PRIMARY KEY,
+  Grade INT,
+  Year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
 
